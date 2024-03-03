aafe65474feee6d5f86ffa65bcf225e4c49409af
Ankit180898 <karan180898@gmail.com>
2024-03-04 00:19:30 +0530

    Added miniplayer and functions and improved the ui

--- lib/Controllers/player_controller.dart
+++ lib/Controllers/player_controller.dart
@@ -1,73 +1,149 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-class PlayerController extends GetxController{
-
-  final audioQuery=OnAudioQuery();
+class PlayerController extends GetxController {
+  final audioQuery = OnAudioQuery();
   var playlist = <SongModel>[].obs;
-  final audioPlayer=AudioPlayer();
-  var playingIndex=0.obs;
-  var isPlaying=false.obs;
+  final audioPlayer = AudioPlayer();
+  var playingIndex = 0.obs;
+  var isPlaying = false.obs;
+  var isLoading = true.obs;
+  var shuffleMode = false.obs;
+  var currentPosition = Duration.zero.obs;
+  var totalDuration = Duration.zero.obs; // Track total duration
+
+  late StreamSubscription<Duration> _positionSubscription;
 
   @override
   void onInit() {
-    // TODO: implement onInit
     super.onInit();
     checkPermissions();
     fetchSongs();
+    setupAudioPlayerListeners();
   }
 
-   checkPermissions() async{
-     // Request necessary permissions
-     Map<Permission, PermissionStatus> statuses = await [
-       Permission.audio,
-       Permission.storage,
-       Permission.manageExternalStorage,
-     ].request();
-    if(statuses.values.every((status) => status.isGranted)){
-      return ;
-    }
-    else{
+  checkPermissions() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.audio,
+      Permission.storage,
+      Permission.manageExternalStorage,
+    ].request();
+    if (statuses.values.every((status) => status.isGranted)) {
+      return;
+    } else {
       checkPermissions();
     }
   }
+
   void fetchSongs() async {
-    // Initialize the audio query instance
-    // Fetch all songs
-    final songs = await audioQuery.querySongs(
-      ignoreCase: true,
-      orderType: OrderType.ASC_OR_SMALLER,
-      sortType: null,
-      uriType: UriType.EXTERNAL
-    );
-    // Clear existing playlist
-    playlist.clear();
-    // Map audio_query SongInfo objects to our Song model
-    playlist.addAll(songs);
-    if (kDebugMode) {
-      print("my songs: $playlist");
+    isLoading.value = true;
+    try {
+      final songs = await audioQuery.querySongs(
+        ignoreCase: true,
+        orderType: OrderType.ASC_OR_SMALLER,
+        sortType: null,
+        uriType: UriType.EXTERNAL,
+      );
+      playlist.clear();
+      playlist.addAll(songs);
+      isLoading.value = false;
+      if (kDebugMode) {
+        print("my songs: $playlist");
+      }
+    } catch (e) {
+      isLoading.value = false;
+      print(e.toString());
     }
   }
 
-  playSongs(String? url,int index){
-    playingIndex.value=index;
+  playSongs(String? url, int index) {
+    playingIndex.value = index;
     try {
       audioPlayer.setAudioSource(
-      AudioSource.uri(Uri.parse(url!)),
+        AudioSource.uri(Uri.parse(url!)),
       );
       audioPlayer.play();
-      isPlaying.value=true;
-    } on Exception catch (e) {
+      isPlaying.value = true;
+    } catch (e) {
       print(e.toString());
-      // TODO
     }
-  }
-  pauseSong(){
-    audioPlayer.pause();
-    isPlaying.value=false;
+
+    _positionSubscription = audioPlayer.positionStream.listen((position) {
+      currentPosition.value = position;
+    });
+
+    // Get total duration when audio is loaded
+    audioPlayer.durationStream.listen((duration) {
+      totalDuration.value = duration ?? Duration.zero;
+    });
   }
 
+  pauseSong() {
+    audioPlayer.pause();
+    isPlaying.value = false;
+  }
+
+  void shuffleSongs() {
+    playlist.value = playlist.toList()
+      ..shuffle(Random());
+    playingIndex.value = 0;
+    playSongs(playlist[0].uri, 0);
+    shuffleMode.value = true; // Set shuffle mode to true after shuffling
+  }
+
+
+  playSongOrShuffle(int index) {
+    if (shuffleMode.value) {
+      playShuffledSongs();
+    } else {
+      playSongs(playlist[index].uri, index);
+    }
+  }
+
+  playShuffledSongs() {
+    playlist.shuffle();
+    playingIndex.value = 0;
+    playSongs(playlist[playingIndex.value].uri, playingIndex.value);
+  }
+
+  void selectSong(int index) {
+    playingIndex.value = index;
+    shuffleMode.value = false;
+    playSongs(playlist[index].uri, index);
+  }
+  void playPreviousSong() {
+    int previousIndex = playingIndex.value - 1;
+    if (previousIndex < 0) {
+      previousIndex = playlist.length - 1; // Wrap around to the last song
+    }
+    playSongs(playlist[previousIndex].uri, previousIndex);
+  }
+
+  void playNextSong() {
+    if (shuffleMode.value) {
+      playShuffledSongs();
+    } else {
+      int nextIndex = playingIndex.value + 1;
+      if (nextIndex < playlist.length) {
+        selectSong(nextIndex);
+      } else {
+        // Reached the end of the playlist
+        // You can handle this scenario based on your app's requirements
+      }
+    }
+  }
+
+  void setupAudioPlayerListeners() {
+    audioPlayer.playerStateStream.listen((playerState) {
+      if (playerState.processingState == ProcessingState.completed) {
+        playNextSong();
+      }
+    });
+  }
 }
--- lib/Views/home/home_screen.dart
+++ lib/Views/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:miniplayer/miniplayer.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import '../../Controllers/player_controller.dart';
 import 'package:beat_buddy/Res/constants.dart';
@@ -11,23 +13,111 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var controller = Get.put(PlayerController());
 
-    return Scaffold(
-      backgroundColor: Colors.black12,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black12,
+        // appBar: AppBar(
+        //   backgroundColor: Colors.black,
+        // ),
+        body: Obx(() {
+          if (controller.isLoading.value) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return Stack(
+              children: [
+                ListView.builder(
+                  itemBuilder: (context, index) {
+                    var song = controller.playlist[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Card(
+                        color: darkColor,
+                        child: ListTile(
+                          leading: QueryArtworkWidget(
+                            id: song.id,
+                            type: ArtworkType.AUDIO,
+                            nullArtworkWidget: const Icon(
+                              Icons.music_note_rounded,
+                              size: 32,
+                            ),
+                          ),
+                          title: Text(
+                            song.title ?? '',
+                            maxLines: 1,
+                            style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                              color: bodyTextColor,
+                              letterSpacing: 1.2,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                            song.artist ?? '',
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(color: Colors.grey, wordSpacing: 2, fontSize: 16,),
+                          ),
+                          onTap: () {
+                            controller.playSongOrShuffle(index); // Play song or start shuffling
+                          },
+                          trailing: Obx(() =>
+                              IconButton(
+                                icon: controller.playingIndex.value == index && controller.isPlaying.value==true
+                                    ? const Icon(Icons.pause_rounded)
+                                    : const Icon(Icons.play_arrow_rounded),
+                                onPressed: () {
+                                  if (controller.playingIndex.value == index && controller.isPlaying.value==true) {
+                                    controller.pauseSong();
+                                  } else {
+                                    controller.playSongs(song.uri, index);
+                                  }
+                                },
+                              ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: controller.playlist.length,
+                ),
+                Positioned(
+                  right: 16,
+                bottom: 100,
+                child: FloatingActionButton(
+                  onPressed: () {
+                    controller.shuffleMode.toggle(); // Toggle shuffle mode
+                    if (controller.shuffleMode.value) {
+                      controller.shuffleSongs(); // Start playing shuffled songs if shuffle mode is enabled
+                    }
+                  },
+                  child: Icon(Icons.shuffle_rounded),
+                ),),
+                buildMiniPlayer(controller, controller.playingIndex.value, context),
+              ],
+            );
+          }
+        }),
       ),
-      body: Obx(() => Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                var song = controller.playlist[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Card(
-                    color: darkColor,
-                    child: ListTile(
-                      leading: QueryArtworkWidget(
+    );
+  }
+
+  Widget buildMiniPlayer(PlayerController controller, int index, BuildContext context) {
+    var song = controller.playlist[index];
+    return Miniplayer(
+      backgroundColor: Colors.black,
+      minHeight: MediaQuery.of(context).size.height*0.10,
+      maxHeight: MediaQuery.of(context).size.height,
+      builder: (height, percentage) {
+        if (height <= MediaQuery.of(context).size.height*0.20) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Obx(()=>
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      QueryArtworkWidget(
                         id: song.id,
                         type: ArtworkType.AUDIO,
                         nullArtworkWidget: const Icon(
@@ -35,44 +125,125 @@ class HomeScreen extends StatelessWidget {
                           size: 32,
                         ),
                       ),
-                      title: Text(
-                        song.title ?? '',
-                        maxLines: 1,
-                        style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                          color: bodyTextColor,
-                          letterSpacing: 1.2,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              song.title ?? '',
+                              maxLines: 1,
+                              style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                                color: bodyTextColor,
+                                letterSpacing: 1.2,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              song.artist ?? '',
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(color: Colors.grey, wordSpacing: 2, fontSize: 16,),
+                            ),
+                          ],
                         ),
                       ),
-                      subtitle: Text(
-                        song.artist ?? '',
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(color: Colors.grey, wordSpacing: 2, fontSize: 16,),
+                      IconButton(
+                        icon: controller.isPlaying.value ? const Icon(Icons.pause_rounded) : const Icon(Icons.play_arrow_rounded),
+                        onPressed: () {
+                          if (controller.isPlaying.value) {
+                            controller.pauseSong();
+                          } else {
+                            controller.playSongs(song.uri, index);
+                          }
+                        },
                       ),
-                      trailing: Obx(()=>
-                        IconButton(
-                          icon: controller.playingIndex.value == index && controller.isPlaying.value==true
-                              ? const Icon(Icons.pause_rounded)
-                              : const Icon(Icons.play_arrow_rounded),
-                          onPressed: () {
-                            if (controller.playingIndex.value == index && controller.isPlaying.value==true) {
-                              controller.pauseSong();
-                            } else {
-                              controller.playSongs(song.uri, index);
-                            }
-                          },
-                        ),
+
+                    ],
+                  ),
+                     Container(
+                      height: 2,
+                      width: MediaQuery.of(context).size.width - 100, // Adjust the width as needed
+                      margin: EdgeInsets.symmetric(vertical: 8), // Add margin for positioning
+                      child: Slider(
+                        allowedInteraction: null,
+                        value: controller.currentPosition.value.inSeconds.toDouble(),
+                        min: 0,
+                        max: controller.totalDuration.value.inSeconds.toDouble(),
+                        onChanged: (value) {
+                          // Seek to the specified position when slider value changes
+                           controller.audioPlayer.seek(Duration(seconds: value.toInt()));
+                        },
+                        // onChangeEnd: (value) {
+                        //   controller.audioPlayer.seek(Duration(seconds: value.toInt()));
+                        // },
                       ),
                     ),
-                  ),
-                );
-              },
-              itemCount: controller.playlist.length,
+
+                ],
+              ),
             ),
-          ),
-        ],
-      )),
+          );
+        } else {
+          return SingleChildScrollView(
+            child: Obx(()=>
+               Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Center(
+                      child: Container(height: 200,width: 200,decoration: BoxDecoration(
+                        color: secondaryColor,
+                        borderRadius: BorderRadius.circular(24),
+                      ),child: Icon(Icons.music_note_rounded,size: 100,)),
+                    ),
+                  ),
+                  Text(song.title),
+                  Text(song.artist.toString()),
+                  Slider(
+                    value: controller.currentPosition.value.inSeconds.toDouble(),
+                    min: 0,
+                    max: controller.totalDuration.value.inSeconds.toDouble(),
+                    onChanged: (value) {},
+                    onChangeEnd: (value) {
+                      controller.audioPlayer.seek(Duration(seconds: value.toInt()));
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.skip_previous),
+                        onPressed: () {
+                          controller.playPreviousSong();
+                        },
+                      ),
+                      IconButton(
+                        icon: controller.isPlaying.value ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+                        onPressed: () {
+                          if (controller.isPlaying.value) {
+                            controller.pauseSong();
+                          } else {
+                            controller.playSongs(song.uri, index);
+                          }
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.skip_next),
+                        onPressed: () {
+                          controller.playNextSong();
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
